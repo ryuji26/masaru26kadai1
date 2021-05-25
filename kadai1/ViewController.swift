19dd3fc47b3102ad04c17de03f9a485aeb056c88
ryuji26 <ryujispjp@gmail.com>
2021-05-25 17:40:12 +0900

    チャレンジ

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func pressButton(_ sender: Any) {
+        //textFieldの型を変換
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
+        let number3 = Int(textField3.text ?? "") ?? 0
+        let number4 = Int(textField4.text ?? "") ?? 0
+        let number5 = Int(textField5.text ?? "") ?? 0
+        
+        //計算
+        let result = number1 + number2 + number3 + number4 + number5
+        
+        //計算結果をラベルに表示
+        resultLabel.text = "\(result)"
     }
-
-
 }
-
